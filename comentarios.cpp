72a84eca2240d5248f56f3e26a992e8fb5e896fd
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-08-27 18:14:41 +0000

    comentarios com // e /* */

---
+++ comentarios.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+  // Comentários de uma linha ignoram somente o que está escrito na mesma linha depois do '//'
+  std::cout << "Olá";
+
+  /* comentários MUlti-linha
+  std:: cout << "Tudo que estiver aqui dentro será ignorado";
+  std:: cout << "Ou seja, apenas o "olá" anterior será mostrado na saida do código";
+  */
+}
+ 
